a4dfffbc6efa8dda0082903e7a17770260d6d315
Igor <igormraesmm@gmail.com>
2023-12-04 15:44:08 -0300

    feat(homepage): update home style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,15 @@
 export function Header() {
   return (
     <>
-      <header className="h-28 flex items-center justify-between px-16 font-poppins ">
+      <header className="h-28 flex items-center justify-between px-16 font-poppins font-bold">
         <img className="h-28" src="./logo.png" alt="logo" />
-        <div>
-          <ul className="flex items-center gap-5 text-white">
-            <li className="hover:underline">
-              <a href="/login">Login</a>
-            </li>
-            <li className="hover:underline">
-              <a href="/register">Register</a>
-            </li>
-          </ul>
+        <div className=" flex items-center gap-2">
+          <button className="text-white border-2 px-7 py-2 rounded hover:bg-white hover:text-black duration-300">
+            Login
+          </button>
+          <button className="text-black bg-white px-7 py-2.5 rounded hover:scale-105 duration-300">
+            Sign Up
+          </button>
         </div>
       </header>
     </>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,29 +1,3 @@
 export function Home() {
-  return (
-    <>
-      <div className="xl:flex xl:items-center xl:justify-center gap-48 px-16">
-        <div className="relative flex items-center justify-center flex-col gap-4 ">
-          <div className="absolute inset-0 bg-zinc-700 rounded-full blur-3xl h-64"></div>
-          <h1 className="text-white lg:text-5xl text-center font-lora italic text-4xl relative">
-            Not just a car, but your <span className="underline">passion</span>
-          </h1>
-          <p className="text-white max-w-lg text-center mt-5 text-lg xl:text-2xl  relative font-poppins italic">
-            "Life is a journey, enjoy the ride. Cars aren't just machines;
-            they're vessels that carry our dreams, passions, and the stories of
-            our adventures."
-          </p>
-        </div>
-        <div className="flex items-center justify-center">
-          <div className="min-w-full">
-            <img
-              className="md:w-[864px] md:h-[550px] mt-28"
-              src="./car-homepage.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
-      <section className="w-full flex items-center flex-col mt-10 gap-5"></section>
-    </>
-  )
+  return <></>
 }
