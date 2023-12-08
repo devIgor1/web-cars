0b62806e6d9956188cff83be10dc562820824e22
Igor <igormraesmm@gmail.com>
2023-12-08 11:56:20 -0300

    style(dashboard): input image style

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
         path: "/dashboard/new",
         element: (
           <Private>
-            <New />,
+            <New />
           </Private>
         ),
       },
--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,12 +1,28 @@
 import Container from "../../../components/container"
 import { DashboardHeader } from "../../../components/panelHeader"
+import { MdOutlineFileUpload } from "react-icons/md"
 
 export function New() {
   return (
-    <div>
-      <Container>
-        <DashboardHeader />
-      </Container>
-    </div>
+    <Container>
+      <DashboardHeader />
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-5">
+        <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32">
+          <div className="absolute cursor-pointer">
+            <MdOutlineFileUpload size={30} />
+          </div>
+          <div>
+            <input
+              type="file"
+              accept="image/*"
+              className=" cursor-pointer bg-black h-32 opacity-0"
+            />
+          </div>
+        </button>
+      </div>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center mt-2"></div>
+    </Container>
   )
 }
