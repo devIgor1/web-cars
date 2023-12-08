5529c5277acedac20ed5fd02302c94b054348031
Igor <igormraesmm@gmail.com>
2023-12-08 08:41:04 -0300

    feat: add AuthContext and AuthProvider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,10 @@
+import { onAuthStateChanged } from "firebase/auth"
 import { ReactNode, createContext, useState, useEffect } from "react"
+import { auth } from "../services/firebaseConnection"
 
 type AuthContextData = {
   signed: boolean
+  loadingAuth: boolean
 }
 
 interface AuthProviderProps {
@@ -18,9 +21,30 @@ export const AuthContext = createContext({} as AuthContextData)
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps | null>(null)
+  const [loadingAuth, setLoadingAuth] = useState<boolean>(true)
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({
+          uid: user.uid,
+          name: user?.displayName,
+          email: user?.email,
+        })
+        setLoadingAuth(false)
+      } else {
+        setUser(null)
+        setLoadingAuth(false)
+      }
+    })
+
+    return () => {
+      unsub
+    }
+  }, [])
 
   return (
-    <AuthContext.Provider value={{ signed: !!user }}>
+    <AuthContext.Provider value={{ signed: !!user, loadingAuth }}>
       {children}
     </AuthContext.Provider>
   )
