1fe9e8c11418bfd9e1687808684658c1af6230dc
Igor <igormraesmm@gmail.com>
2023-12-11 17:08:16 -0300

    style(carDetail): add slider

--- src/pages/car/index.tsx
+++ src/pages/car/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { FaWhatsapp } from "react-icons/fa"
 import { useParams } from "react-router-dom"
 import { db } from "../../services/firebaseConnection"
+import { Swiper, SwiperSlide } from "swiper/react"
 
 interface CarProps {
   id: string
@@ -30,6 +31,7 @@ interface CarImageProps {
 export function CarDetail() {
   const { id } = useParams()
   const [car, setCar] = useState<CarProps>()
+  const [sliderPreview, setSliderPreview] = useState<number>(2)
 
   useEffect(() => {
     async function loadCar() {
@@ -60,49 +62,82 @@ export function CarDetail() {
     loadCar()
   }, [id])
 
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 720) {
+        setSliderPreview(1)
+      } else {
+        setSliderPreview(2)
+      }
+    }
+
+    handleResize()
+
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
+
   return (
     <Container>
-      <h1>Slider</h1>
+      <div className="rounded- w-full bg-white rounded-lg p-6 my-4 font-poppins">
+        <Swiper
+          slidesPerView={sliderPreview}
+          pagination={{ clickable: true }}
+          navigation
+        >
+          {car?.images.map((image) => (
+            <SwiperSlide key={image.name}>
+              <img
+                src={image.url}
+                className="w-full h-96 object-cover mb-9 rounded-lg"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-      {car && (
-        <main className="w-full bg-white rounded-lg p-6 my-4 font-poppins">
-          <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
-            <h1 className="font-bold text-3xl">{car?.name}</h1>
-            <h1 className="font-bold text-3xl">${car?.price}</h1>
-          </div>
-          <p className="text-lg">{car.model}</p>
+        {car && (
+          <main>
+            <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
+              <h1 className="font-bold text-3xl">{car?.name}</h1>
+              <h1 className="font-bold text-3xl">${car?.price}</h1>
+            </div>
+            <p className="text-lg">{car.model}</p>
 
-          <div className="flex w-full gap-6 my-4">
-            <div className="flex flex-col gap-4">
-              <div>
-                <p>City</p>
-                <strong>{car?.city}</strong>
+            <div className="flex w-full gap-6 my-4">
+              <div className="flex flex-col gap-4">
+                <div>
+                  <p>City</p>
+                  <strong>{car?.city}</strong>
+                </div>
+                <div>
+                  <p>Year</p>
+                  <strong>{car?.year}</strong>
+                </div>
               </div>
-              <div>
-                <p>Year</p>
-                <strong>{car?.year}</strong>
+              <div className="flex flex-col gap-4">
+                <div>
+                  <p>Km</p>
+                  <strong>{car?.km}</strong>
+                </div>
               </div>
             </div>
-            <div className="flex flex-col gap-4">
-              <div>
-                <p>Km</p>
-                <strong>{car?.km}</strong>
-              </div>
-            </div>
-          </div>
-          <strong>Description</strong>
-          <p className="mb-4">{car?.description}</p>
-          <strong>Phone</strong>
-          <p className="mb-4">{car?.phone}</p>
-          <a className="bg-green-500 w-full p-3 text-white flex items-center justify-center gap-2 my-6 h-14 text-center md:h-11 text-base md:text-xl rounded-lg cursor-pointer font-bold hover:bg-green-400 duration-300">
-            Interested? Reach out to the seller for details and purchase
-            options.
-            <span>
-              <FaWhatsapp size={26} />
-            </span>
-          </a>
-        </main>
-      )}
+            <strong>Description</strong>
+            <p className="mb-4">{car?.description}</p>
+            <strong>Phone</strong>
+            <p className="mb-4">{car?.phone}</p>
+            <a className="bg-green-500 w-full p-3 text-white flex items-center justify-center gap-2 my-6 h-14 text-center md:h-11 text-base md:text-xl rounded-lg cursor-pointer font-bold hover:bg-green-400 duration-300">
+              Interested? Reach out to the seller for details and purchase
+              options.
+              <span>
+                <FaWhatsapp size={26} />
+              </span>
+            </a>
+          </main>
+        )}
+      </div>
     </Container>
   )
 }
